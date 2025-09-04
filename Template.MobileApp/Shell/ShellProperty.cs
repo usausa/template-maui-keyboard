95a044e469fb13baa03df71768ea6d262d0edcc2
usausa <machi.pon@gmail.com>
2025-09-04 10:02:00 +0900

    Update baseline

--- Template.MobileApp/Shell/ShellProperty.cs
+++ Template.MobileApp/Shell/ShellProperty.cs
@@ -1,5 +1,9 @@
 namespace Template.MobileApp.Shell;
 
+using Microsoft.Maui.Controls.Shapes;
+
+using Smart.Maui.Interactivity;
+
 public static class ShellProperty
 {
     // ------------------------------------------------------------
@@ -165,6 +169,59 @@ public static class ShellProperty
             shell.Function2Enabled = GetFunction2Enabled(bindable);
             shell.Function3Enabled = GetFunction3Enabled(bindable);
             shell.Function4Enabled = GetFunction4Enabled(bindable);
+        }
+    }
+
+    // ------------------------------------------------------------
+    // BusyOverlay
+    // ------------------------------------------------------------
+
+    public static readonly BindableProperty BusyOverlayProperty = BindableProperty.CreateAttached(
+        "BusyOverlay",
+        typeof(bool),
+        typeof(ShellProperty),
+        false,
+        propertyChanged: OnBusyOverlayChanged);
+
+    public static bool GetBusyOverlay(BindableObject obj) =>
+        (bool)obj.GetValue(BusyOverlayProperty);
+
+    public static void SetBusyOverlay(BindableObject obj, bool value) =>
+        obj.SetValue(BusyOverlayProperty, value);
+
+    private static void OnBusyOverlayChanged(BindableObject bindable, object? oldValue, object? newValue)
+    {
+        if (bindable is not Rectangle view)
+        {
+            return;
+        }
+
+        if (oldValue is not null)
+        {
+            var behavior = view.Behaviors.FirstOrDefault(static x => x is BusyOverlayBehavior);
+            if (behavior is not null)
+            {
+                view.Behaviors.Remove(behavior);
+            }
+        }
+
+        if (newValue is not null)
+        {
+            view.Behaviors.Add(new BusyOverlayBehavior());
+        }
+    }
+
+    private sealed class BusyOverlayBehavior : BehaviorBase<Rectangle>
+    {
+        protected override void OnAttachedTo(Rectangle bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.InputTransparent = false;
+            bindable.BackgroundColor = Colors.Transparent;
+            bindable.ZIndex = 1000;
+
+            bindable.GestureRecognizers.Add(new TapGestureRecognizer());
         }
     }
 }
